f95351feba50803a7259f6c804ca4295f732cb61
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2020-12-04 03:21:51 +0400

    slide block

---
+++ src/js/slider.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView:1,
+    spaceBetween: 100,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+});
