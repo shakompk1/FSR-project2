37a503bd9d72224284ba3b1819b3387440cee12d
Шахрияр Мирзаи <61832690+shakompk1@users.noreply.github.com>
2020-12-04 12:57:15 +0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/header.scss'
 import './sass/main.scss';
-import './sass/style.scss';
+import './sass/aboutus.scss';
+import './sass/countactus.scss';
 import './sass/slider.scss'
 import './js/slider'
